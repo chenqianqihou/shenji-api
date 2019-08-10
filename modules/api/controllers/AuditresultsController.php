8f31638a4aee1f0c32821ea8cb8192d5bf3fa1eb
Liuyanglong <liuyanglong2137@qq.com>
2019-08-10 21:20:23 +0800

    add auditresult model

---
+++ modules/api/controllers/AuditresultsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\modules\api\controllers;
+
+use app\classes\BaseController;
+use app\classes\ErrorDict;
+use Yii;
+
+class AuditresultsController extends BaseController
+{
+    public function actionList()
+    {
+        $this->defineMethod = 'GET';
+        $this->defineParams = array (
+        );
+        $error = ErrorDict::getError(ErrorDict::SUCCESS);
+        $ret = $this->outputJson($this->getDistricts(), $error);
+        return $ret;
+    }
+}
