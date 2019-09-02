0fee0d400820a6a0b7fbc1e926b4a496676b8e0e
Liuyanglong <liuyanglong2137@qq.com>
2019-09-02 10:42:41 +0800

    update

--- classes/BaseController.php
+++ classes/BaseController.php
@@ -156,6 +156,7 @@ class BaseController extends Controller {
         Yii::$app->response->statusCode = $statusCode;
         Yii::$app->response->format = Response::FORMAT_JSON;
         Yii::$app->response->headers->add('Access-Control-Allow-Origin', '*');
+        Yii::$app->response->headers->add('Access-Control-Expose-Headers', 'Content-Disposition');
         Yii::$app->response->headers->add('Access-Control-Allow-Headers', 'content-type,AUTHORIZATION');
         Yii::$app->response->headers->add('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE');
         return $ret;
