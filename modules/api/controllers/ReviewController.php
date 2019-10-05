d2ea345d042343327deab7dd734501536140e9b1
RyuGou <376832293@qq.com>
2019-10-05 12:12:13 +0800

    pid bug fix

--- modules/api/controllers/ReviewController.php
+++ modules/api/controllers/ReviewController.php
@@ -578,7 +578,7 @@ class ReviewController extends BaseController{
             ->from('auditresults')
             ->innerJoin('project', 'project.id = auditresults.projectid')
             ->innerJoin('people', 'people.id = auditresults.peopleid')
-            ->select('auditresults.id, project.projectnum, project.name, project.projyear, project.projlevel, people.id as pid, people.name as pname, auditresults.status, auditresults.projectid')
+            ->select('auditresults.id, project.projectnum, project.name, project.projyear, project.projlevel, people.pid as pid, people.name as pname, auditresults.status, auditresults.projectid')
             ->where(['auditresults.operatorid' => $user['id']]);
 
         if($query){
