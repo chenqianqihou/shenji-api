dda138b6d97768c8a1f89e91c05d0122832319a5
gonglixin <gonglixin@lianjia.com>
2019-12-26 15:57:49 +0800

    u

--- modules/api/controllers/AuditresultsController.php
+++ modules/api/controllers/AuditresultsController.php
@@ -261,7 +261,7 @@ class AuditresultsController extends BaseController
         $arService = new AuditresultsService();
         if (in_array('审计组成员', $roleList)) {
             $arList = $arService->getAuditResultsList($peopleid, $projectid,$status,$start,$length );
-        }elseif ($this->userInfo['organid'] = '1012') {
+        }elseif ($this->userInfo['organid'] == '1012') {
             $arList = $arService->getAuditResultsAllList($projectid,$status,$start,$length);
         }elseif (in_array('厅/局领导', $roleList) || in_array('项目计划管理人员', $roleList)
             || in_array('法规部门', $roleList) || in_array('审理部门', $roleList)) {
